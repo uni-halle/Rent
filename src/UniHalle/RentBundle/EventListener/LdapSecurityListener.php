39e6a1c1f148567fd99b0a07f1d76c3af1f68848
Daniel Kabel <info@kvibes.de>
2013-12-03 20:33:28 +0100

    Throw AuthenticationException instead Exception

--- src/UniHalle/RentBundle/EventListener/LdapSecurityListener.php
+++ src/UniHalle/RentBundle/EventListener/LdapSecurityListener.php
@@ -41,22 +41,22 @@ class LdapSecurityListener implements EventSubscriberInterface
         $user = $em->getRepository('RentBundle:User')->findOneByUsername($user->getUsername());
         if (!$user) {
             $this->session->getFlashBag()->add('error', 'Sie sind noch nicht für den Geräteverleih registriert.');
-            throw new \Exception('Authentication error');
+            throw new AuthenticationException('Authentication error');
         }
 
         if ($user->getStatus() == UserStatusType::WAITING) {
             $this->session->getFlashBag()->add('error', 'Ihr Nutzerkonto wurde noch nicht freigeschaltet.');
-            throw new \Exception('Authentication error');
+            throw new AuthenticationException('Authentication error');
         }
 
         if ($user->getStatus() == UserStatusType::DISABLED) {
             $this->session->getFlashBag()->add('error', 'Ihr Nutzerkonto wurde deaktiviert.');
-            throw new \Exception('Authentication error');
+            throw new AuthenticationException('Authentication error');
         }
 
         if ($user->getStatus() != UserStatusType::ACTIVE) {
             $this->session->getFlashBag()->add('error', 'Ein Fehler ist bei der Autorisierung aufgetreten.');
-            throw new \Exception('Authentication error');
+            throw new AuthenticationException('Authentication error');
         }
     }
 }
